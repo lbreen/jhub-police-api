4402e15b0dc0df7e45c7b804bd07534509510e23
Luke Breen <l.breen1@outlook.com>
2023-01-06 23:20:46 +0000

    created homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
   def home
+    @category_array = [
+      {
+        image_path: "senior_police",
+        description: "Find out about Senior Leadership in your area",
+        button: "Find out more"
+      },
+      {
+        image_path: "neighbourhood_policing",
+        description: "Get to know your local officers",
+        button: "Find out more"
+      },
+      {
+        image_path: "crime_data",
+        description: "Discover local crime statistics",
+        button: "Find out more"
+      },
+    ]
   end
 end
