e47e37283209ff9c48db9079cc21bdb1990dae26
Luke Breen <l.breen1@outlook.com>
2023-01-06 23:29:11 +0000

    created routes for 3 x pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,20 +2,32 @@ class PagesController < ApplicationController
   def home
     @category_array = [
       {
+        url_path: 'senior_leadership',
         image_path: "senior_police",
         description: "Find out about Senior Leadership in your area",
         button: "Find out more"
       },
       {
+        url_path: 'neighbourhood_policing',
         image_path: "neighbourhood_policing",
         description: "Get to know your local officers",
         button: "Find out more"
       },
       {
+        url_path: 'crime_data',
         image_path: "crime_data",
         description: "Discover local crime statistics",
         button: "Find out more"
       },
     ]
+  end
+
+  def senior_leadership
+  end
+
+  def neighbourhood_policing
+  end
+
+  def crime_data
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'senior_leadership', to: 'pages#senior_leadership'
+  get 'neighbourhood_policing', to: 'pages#neighbourhood_policing'
+  get 'crime_data', to: 'pages#crime_data'
 end
