7d9412494ea32ab96f1aed65f8c0795f8d34a247
Luke Breen <l.breen1@outlook.com>
2023-01-07 16:30:03 +0000

    Seeds.rb seeds all police force data and a list of neighbourhoods. Unwilling to seed neighbourhood data as it requires 4763 api calls to fulfill all instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ require 'rest-client'
 Neighbourhood.destroy_all
 Force.destroy_all
 
+total_start_time = Time.now # Record overall start time
+
 # Fetch a list of police forces from the api and parse the results into an array
 forces = JSON.parse(RestClient.get("https://data.police.uk/api/forces"))
 
@@ -17,10 +19,11 @@ end
 #Confirm creation of force instances
 puts "Created #{Force.count} new police forces"
 
-
 i = 0 # Init API limit counter
 # Iterate over each police force to grab specific data
 Force.all.each do |force|
+  start_time = Time.now
+
   puts "Fetching data for #{force.name}" # User confirmation the api call is progressing
 
   force_data = JSON.parse(RestClient.get("https://data.police.uk/api/forces/#{force.api_id}"))
@@ -41,9 +44,70 @@ Force.all.each do |force|
     end
   end
 
-  if i % 15 == 0
-    sleep(2) # Delay for 2 seconds every 15 API calls to prevent exceeding limit
+  neighbourhood_list = JSON.parse(RestClient.get("https://data.police.uk/api/#{force.api_id}/neighbourhoods"))
+
+  # List all neighbourhoods in the police force and create neighbourhood instances
+  neighbourhood_list.each do |neighbourhood|
+    n = Neighbourhood.new({
+      api_id: neighbourhood["id"],
+      name: neighbourhood["name"],
+    })
+
+    n.force = force # Assign the neighbourhood to a force
+    n.save
+  end
+  finish_time = Time.now
+
+  puts "#{force.name} has been updated. It took #{(finish_time - start_time).round(2)} seconds."
+  puts "--------"
+  if i % 7 == 0
+    # Delay for 1 second every 15 API calls to prevent exceeding limit
+    # Delay every 7 iterations as 2 x API calls per iteration
+    sleep(1)
   end
 
   i += 1 # Increment API limit counter (see line 21)
 end
+
+total_finish_time = Time.now
+
+puts"-----------------------"
+puts "DB seeding complete. "
+puts "#{Force.count} Police Forces and #{Neighbourhood.count} Neighbourhoods have been seeded."
+puts "Seeding took #{(total_finish_time - total_start_time).round(2)} seconds"
+# Update list of neighbourhoods - 4763 neighbourhoods! need to implement a quicker method.
+# i = 0 # Init API limit counter
+# Neighbourhood.all.each do |neighbourhood|
+#   puts "Fetching data for the #{neighbourhood.name} neighbourhood within the #{neighbourhood.force.name}"
+#   neighbourhood_data = JSON.parse(RestClient.get("https://data.police.uk/api/#{neighbourhood.force.api_id}/#{neighbourhood.api_id}"))
+
+#   neighbourhood.update({
+#     url: neighbourhood_data["url"],
+#     lat: neighbourhood_data["centre"]["latitude"],
+#     long: neighbourhood_data["centre"]["longitude"],
+#     description: neighbourhood_data["description"]
+#   })
+
+#   if i % 15 == 0
+#     # Delay for 1 second every 15 API calls to prevent exceeding limit
+#     # Delay every 7 iterations as 2 x API calls per iteration
+#     sleep(1)
+#   end
+
+#   i = 0
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
