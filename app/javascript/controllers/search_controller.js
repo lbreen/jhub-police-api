cdd53d1223942c8e61c124c58902b551d7993ecd
Luke Breen <l.breen1@outlook.com>
2023-01-07 14:08:57 +0000

    Adding search controller for JS

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,8 +1,31 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Connects to data-controller="search"
 export default class extends Controller {
+  static targets = ["input", "results"]
 
   connect() {
-    console.log("Hello from our first Stimulus controller")
+
+  }
+
+  search(event) {
+    // Prevent event default
+    event.preventDefault();
+
+    // Clear any previous results
+    this.resultsTarget.innerHTML = "";
+
+    // Fetch information from api using search data
+    this.fetchData(this.inputTarget.value);
+  }
+
+  fetchData(query) {
+    // Query api with search data
+    fetch(`https://data.police.uk/api/${query}/`)
+    // Extract relevant api data
+  }
+
+  displayResults() {
+    // Display api data in results div
   }
 }
